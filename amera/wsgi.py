91ba6b9df520ddb5de2ff23cc0c72c5ee8f05a77
MeRa <amera.elsayed6@gmail.com>
2015-11-03 20:19:59 +0200

    tes t

--- amera/wsgi.py
+++ amera/wsgi.py
@@ -18,7 +18,7 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amera.settings")
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
 
 # application = Cling(get_wsgi_application())
 # application = get_wsgi_application()
